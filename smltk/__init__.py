d0744f02f95a2ef91324305743ae4f701c0d358f
Alessandra Bilardi <alessandra.bilardi@pixartprinting.com>
2021-10-26 00:09:04 +0200

    step 3 - added init files of package and tests

---
+++ smltk/__init__.py
@@ -0,0 +1,19 @@
+"""Simple Machine Learning Tool Kit package
+
+This package contains the modules to simplify your code for your machine learning processes.
+
+It is part of the educational repositories (https://github.com/pandle/materials)
+to learn how to write stardard code and common uses of the TDD.
+
+Package contents two classes to manage data preprocessing, testing and reporting.
+
+    >>> import smltk
+    >>> help(smltk)
+    >>> import smltk.metrics as Metrics
+    >>> help(Metrics)
+
+# license MIT
+# support https://github.com/bilardi/smltk/issues
+"""
+__version__ = '0.0.1'
+__author__ = 'Alessandra Bilardi'
